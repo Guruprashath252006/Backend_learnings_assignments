c8b1e30e8ea84e5d0afedf1519ae375e19296733
guru <guruprashath25@gmail.com>
2026-04-16 23:42:50 +0530

    updated

--- src/Node/day7/config/db.js
+++ src/Node/day7/config/db.js
@@ -2,20 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
 
-    
 
     try {
-        // eslint-disable-next-line no-undef
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Database Connected Successfully ${conn.connection.host}`);
-
     } catch (error) {
-        console.log("Error Occured",error);
-        // eslint-disable-next-line no-undef
+        console.log(`Error Occured`,error);
         process.exit(0)
+        
     }
-
 }
 
-
-export default connectDB
+export default connectDB;
